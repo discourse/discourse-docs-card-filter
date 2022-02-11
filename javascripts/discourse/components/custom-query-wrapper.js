6e53f72a8837aed6a89d17959b63ee71d0b42872
hernandoagf <A7XHernando@gmail.com>
2022-02-11 14:46:00 -0300

    Debug count

--- javascripts/discourse/components/custom-query-wrapper.js
+++ javascripts/discourse/components/custom-query-wrapper.js
@@ -64,7 +64,7 @@ export default Component.extend({
     return tag.count >= 1;
   },
 
-  @discourseComputed("router")
+  @discourseComputed("router.get('docs.index')")
   topicCount(count) {
     console.log(count)
     if (count > 1) {
