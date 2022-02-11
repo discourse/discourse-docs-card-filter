78a68e2cd9e6d3f5a69befde162e83f6ea0b0a2d
hernandoagf <A7XHernando@gmail.com>
2022-02-11 14:09:04 -0300

    Log tag object

--- javascripts/discourse/components/tag-wrapper.js
+++ javascripts/discourse/components/tag-wrapper.js
@@ -14,6 +14,7 @@ export default Component.extend({
 
   @action
   selectTag() {
+    console.log(this)
     this.get("router").transitionTo("docs.index", {
       queryParams: {
         tags: this.tag.id,
